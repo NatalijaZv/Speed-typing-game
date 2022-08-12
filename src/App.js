46700549121f49c477e8b3a8ed71cfea50000771
NatalijaZv <natalijazver929@gmail.com>
2022-08-12 10:50:34 +0200

    Code cleaned using costum hook

--- src/App.js
+++ src/App.js
@@ -1,49 +1,9 @@
 import React from "react";
+import useGameLogic from "./useGameLogic";
 
 function App() {
-  // const [count, setCount] = React.useState("")
-  const time = 5
-  const [text, setText] = React.useState("");
-  const [timeRemaining, setTimeRemaining] = React.useState(time)
-  const [gameOn, setGameOn] =React.useState(false)
-  const [wordCount, setWordCount] = React.useState()
-  const textBoxRef = React.useRef(null)
-
-  function handleChange(event) {
-    setText(event.target.value);
-  }
-
-  function calculateWordCount(text) {
-    const wordsArr = text.split(" ");
-    const filteredWords = wordsArr.filter(word => word !== '')
-    return filteredWords.length;
-  }
-  function startGame(){
-    setGameOn(true)
-    setText("")
-    setTimeRemaining(time)
-    setWordCount("")
-    textBoxRef.current.disabled = false
-    textBoxRef.current.focus()
-  }
-  function endGame(){
-    setGameOn(false)
-    setWordCount(calculateWordCount(text))
-  }
-  React.useEffect(function(){
-    if(gameOn===true){
-      if(timeRemaining===0){
-        endGame()
-      }
-      else{
-        setTimeout(()=>{
-          setTimeRemaining(prevTime => {return prevTime - 1})
-        },1000)
-      }
-     
-    }
-  },[timeRemaining,gameOn])
-
+  const {text,gameOn,timeRemaining,wordCount,textBoxRef,startGame,handleChange} = useGameLogic()
+  
   return (  
     <div className="App">
       <h1>Speed typing game</h1>
---
+++ src/useGameLogic.js
@@ -0,0 +1,45 @@
+import {useState, useEffect, useRef} from "react"
+
+export default function useGameLogic(){
+    const time = 5
+    const [text, setText] = useState("");
+    const [timeRemaining, setTimeRemaining] = useState(time)
+    const [gameOn, setGameOn] =useState(false)
+    const [wordCount, setWordCount] = useState()
+    const textBoxRef = useRef(null)
+  
+    function handleChange(event) {
+      setText(event.target.value);
+    }
+  
+    function calculateWordCount(text) {
+      const wordsArr = text.split(" ");
+      const filteredWords = wordsArr.filter(word => word !== '')
+      return filteredWords.length;
+    }
+    function startGame(){
+      setGameOn(true)
+      setText("")
+      setTimeRemaining(time)
+      setWordCount("")
+      textBoxRef.current.disabled = false
+      textBoxRef.current.focus()
+    }
+    function endGame(){
+      setGameOn(false)
+      setWordCount(calculateWordCount(text))
+    }
+    useEffect(function(){
+      if(gameOn===true){
+        if(timeRemaining===0){
+          endGame()
+        }
+        else{
+          setTimeout(()=>{
+            setTimeRemaining(prevTime => {return prevTime - 1})
+          },1000)
+        }
+      }
+    },[timeRemaining,gameOn])
+    return {text,gameOn,timeRemaining,wordCount,textBoxRef,startGame,handleChange}
+}
